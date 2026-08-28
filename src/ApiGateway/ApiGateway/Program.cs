5a315ef8a7c78697774ad075b28cac36ecf1eaa2
sachinthacham <sachinthachamindu26@gmail.com>
2026-08-28 23:36:09 +0530

    update

--- src/ApiGateway/ApiGateway/Program.cs
+++ src/ApiGateway/ApiGateway/Program.cs
@@ -2,15 +2,22 @@ using BuildingBlocks;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
+builder.AddSerilogLogging("API Gateway");
 
 builder.Services.AddServiceDefaults();
+builder.Services.AddPackageDeliveryCors(builder.Configuration);
+builder.Services.AddPackageDeliveryRateLimiting();
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
+app.UsePackageDeliveryRequestLogging();
 app.UseGlobalExceptionHandling();
+
+app.UsePackageDeliveryCors();
+app.UsePackageDeliveryRateLimiting();
 
 app.MapServiceDefaults("API Gateway");
 app.MapReverseProxy();
