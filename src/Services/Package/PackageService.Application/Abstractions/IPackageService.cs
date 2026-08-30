85f861e7a3c81bf65cb5b1a770eac3c9a521e2bc
sachinthacham <sachinthachamindu26@gmail.com>
2026-08-30 21:05:51 +0530

    Extend IPackageService with paging, status update, and cancel operations
    Adds GetPagedAsync, UpdateStatusAsync, and CancelAsync to the
    application service contract ahead of their implementation.

--- src/Services/Package/PackageService.Application/Abstractions/IPackageService.cs
+++ src/Services/Package/PackageService.Application/Abstractions/IPackageService.cs
@@ -1,4 +1,6 @@
+using BuildingBlocks.Common;
 using PackageService.Application.DTOs;
+using PackageService.Domain.Enums;
 
 namespace PackageService.Application.Abstractions;
 
@@ -6,4 +8,7 @@ public interface IPackageService
 {
     Task<PackageResponse> CreateAsync(CreatePackageRequest request, Guid senderId);
     Task<PackageResponse?> GetByIdAsync(Guid id);
+    Task<PaginatedList<PackageResponse>> GetPagedAsync(int pageNumber, int pageSize, PackageStatus? status, Guid? senderId);
+    Task<PackageResponse?> UpdateStatusAsync(Guid id, PackageStatus newStatus);
+    Task<PackageResponse?> CancelAsync(Guid id);
 }
