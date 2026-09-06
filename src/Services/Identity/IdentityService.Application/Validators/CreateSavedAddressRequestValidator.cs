66a73ba5ee0fee28235fb8c99fba19226df4e734
sachinthacham <sachinthachamindu26@gmail.com>
2026-09-06 22:39:57 +0530

    Add saved address validation

---
+++ src/Services/Identity/IdentityService.Application/Validators/CreateSavedAddressRequestValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using IdentityService.Application.Dtos;
+
+namespace IdentityService.Application.Validators;
+
+public class CreateSavedAddressRequestValidator : AbstractValidator<CreateSavedAddressRequest>
+{
+    public CreateSavedAddressRequestValidator()
+    {
+        RuleFor(x => x.Label).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Street).NotEmpty().MaximumLength(200);
+        RuleFor(x => x.City).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.State).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.PostalCode).NotEmpty().MaximumLength(20);
+        RuleFor(x => x.Country).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Latitude).InclusiveBetween(-90, 90).When(x => x.Latitude.HasValue);
+        RuleFor(x => x.Longitude).InclusiveBetween(-180, 180).When(x => x.Longitude.HasValue);
+    }
+}
