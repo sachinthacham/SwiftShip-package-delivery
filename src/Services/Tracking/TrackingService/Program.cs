6e90d869f5b7d575463beb62e801ff39a1c14685
sachinthacham <sachinthachamindu26@gmail.com>
2026-09-14 23:12:27 +0530

    config: configure tracking service

--- src/Services/Tracking/TrackingService/Program.cs
+++ src/Services/Tracking/TrackingService/Program.cs
@@ -2,6 +2,7 @@ using BuildingBlocks;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using TrackingService.API.BackgroundServices;
+using TrackingService.API.Hubs;
 using TrackingService.Application;
 using TrackingService.Application.DTOs;
 using TrackingService.API.Extensions;
@@ -9,9 +10,12 @@ using TrackingService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://0.0.0.0:5004");
+builder.AddSerilogLogging("Tracking Service");
 
 builder.Services.AddJwtAuthentication(builder.Configuration);
 builder.Services.AddServiceDefaults();
+builder.Services.AddPackageDeliveryCors(builder.Configuration);
+builder.Services.AddPackageDeliveryRateLimiting();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
@@ -19,18 +23,27 @@ builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddValidatorsFromAssemblyContaining<AddTrackingRequest>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 builder.Services.AddHostedService<ShipmentCreatedConsumer>();
+builder.Services.AddHostedService<ShipmentStatusChangedConsumer>();
 
 var app = builder.Build();
 
+app.UsePackageDeliveryRequestLogging();
 app.UseGlobalExceptionHandling();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UsePackageDeliveryCors();
+app.UsePackageDeliveryRateLimiting();
+
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapServiceDefaults("Tracking Service");
 app.MapControllers();
+app.MapHub<TrackingHub>("/hubs/tracking");
 
 app.Run();
+
+public partial class Program { }
